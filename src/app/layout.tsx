4a87550aa04113d9e65f9309216fdb519c65c4cd
alex-lin64 <alex_lin@brown.edu>
2024-05-31 17:27:00 -0400

    ready to deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 import styles from "./layout.module.scss";
 import "./globals.scss";
 
